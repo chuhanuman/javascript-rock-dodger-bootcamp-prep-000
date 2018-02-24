5918d49d5542e6e8bc0a8fea9e12a4bd4192e21f
Hanuman Chu <chuhanuman@gmail.com>
2018-02-24 23:53:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function endGame() {
   for(i=0;i<ROCKS.length;i++) {
     ROCKS[i].remove();
   }
-  document.removeEventListener('keydown', moveDodger);
+  window.removeEventListener('keydown', moveDodger);
   return alert("YOU LOSE!");
 }
 
